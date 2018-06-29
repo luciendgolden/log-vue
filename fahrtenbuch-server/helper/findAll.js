7d74bc9046738e2419b716e9b243672579ddb221
Alexander Tampier <alexander.tampier@icloud.com>
2018-06-29 09:54:30 +0200

    added working pdf-creater feature

--- fahrtenbuch-server/helper/findAll.js
+++ fahrtenbuch-server/helper/findAll.js
@@ -3,6 +3,6 @@ const LogEntry = require('../model/logEntry');
 module.exports = () => new Promise((resolve, reject) => {
   LogEntry.find({}).sort({ created: 'desc' }).exec((error, logEntries) => {
     if (error) return reject(error);
-    return resolve({logEntries: logEntries});
+    return resolve({ logEntries });
   });
 });
--- fahrtenbuch-server/helper/logEntryOperations.js
+++
@@ -1,6 +0,0 @@
-const LogEntry = require('../model/logEntry');
-
-module.exports.findAll = callback => LogEntry.find({}).sort({ created: 'desc' }).exec((error, logEntries) => {
-  if (error) return callback({ error });
-  return callback({ logEntries });
-});
